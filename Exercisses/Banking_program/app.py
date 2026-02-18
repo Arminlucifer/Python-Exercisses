363a3e06c030a4806c8708c6ff54af884917816e
Armin <awrminlucifer@gmail.com>
2026-02-19 00:06:49 +0330

    Main app programmed

--- Exercisses/Banking_program/app.py
+++ Exercisses/Banking_program/app.py
@@ -1,6 +1,36 @@
+import deposit
+import check_balance
+import withdraw
+
+
 def main():
-    print
+    balance = 0
+    running = True
+
+    while running:
+        print("=====BANKING PROGRAM======")
+        print()
+        print("What Would You Like To Do?")
+        print()
+        print("1.Check balance")
+        print("2.Withdraw Money")
+        print("3.Deposit Money")
+        print("4.Exit")
+        choice = int(input("Enter your choice (1/2/3/4): "))
+
+        match choice:
+            case 1:
+                check_balance(balance)
+            case 2:
+                withdraw(balance)
+            case 3:
+                deposit(balance)
+            case 4:
+                running = False
+            case _:
+                print("That is not a valid input")
+    print("That you! Have a nice day")
 
 
-if __name__ == '__main___':
+if __name__ == "__main__":
     main()
