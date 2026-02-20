3891eb160117b0c39115659d2b33aac48353d461
Armin <awrminlucifer@gmail.com>
2026-02-21 03:27:21 +0330

    hangman art completed

---
+++ Exercisses/Hangman/app.py
@@ -0,0 +1,48 @@
+import random
+
+words = ("apple", "banana", "grape", "orange", "strawberry")
+
+
+hangman_art = {0: ("   ",
+                   "   ",
+                   "   "),
+               1: (" o ",
+                   "   ",
+                   "   "),
+               2: (" o ",
+                   " | ",
+                   "   "),
+               3: (" o ",
+                   "/| ",
+                   "   "),
+               4: (" o ",
+                   "/|\\ ",
+                   "   "),
+
+               5: (" o ",
+                   "/|\\ ",
+                   "/  "),
+
+               6: (" o ",
+                   "/|\\ ",
+                   "/ \\ ")}
+
+
+def display_art(wrong_guesses):
+    pass
+
+
+def display_hint(hint):
+    pass
+
+
+def display_answer(answer):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
