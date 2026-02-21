ef53ea944e02f736a9874a7e3a355952942dff33
Armin <awrminlucifer@gmail.com>
2026-02-21 04:27:30 +0330

    Hang man game done

--- Exercisses/Hangman/app.py
+++ Exercisses/Hangman/app.py
@@ -36,26 +36,47 @@ def display_art(wrong_guesses):
 
 
 def display_hint(hint):
-    print(" ".join(hint))
+    print("".join(hint))
 
 
 def display_answer(answer):
-    pass
+    print("".join(answer))
 
 
 def main():
-    runnig = True
+    running = True
     answer = random.choice(words)
     wrong_guesses = 0
     guessed_letters = set()
     hint = ["_"] * len(answer)
-    while runnig:
+    while running:
         display_art(wrong_guesses)
         print()
         display_hint(hint)
         print()
         guess = input("Enter a letter: ").lower()
-        
+        if guess == answer:
+            print("YOU WON!")
+            running = False
+        else:
+            if not guess.isalpha() or len(guess) != 1:
+                print("Invalid input")
+                continue
+            if guess in answer:
+                for i in range(len(answer)):
+                    if answer[i] == guess:
+                        hint[i] = guess
+            else:
+                wrong_guesses += 1
+        if not "_" in hint:
+            display_answer(answer)
+            print("YOU WON!")
+            running = False
+        elif wrong_guesses >= len(hangman_art) - 1:
+            print("You've LOST")
+            print("The answer was: ", end=" ")
+            display_answer(answer)
+            running = False
 
 
 if __name__ == "__main__":
